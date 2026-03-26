a14e869be689b2064acd8fd78cff3bf6e8417e07
shravanim100 <shravanimalvade1@gmail.com>
2026-03-26 17:22:56 +0530

    image detector fix

--- backend/ml/ai_detector.py
+++ backend/ml/ai_detector.py
@@ -1,7 +1,6 @@
-import requests
-from PIL import Image
-import io
+import os
 import base64
+from PIL import Image
 
 def encode_image(image_path: str) -> str:
     with open(image_path, "rb") as f:
@@ -10,41 +9,42 @@ def encode_image(image_path: str) -> str:
 def detect_ai_image(image_path: str) -> dict:
     try:
         from transformers import pipeline
-        
+
         detector = pipeline(
             "image-classification",
-            model="umm-maybe/AI-image-detector"
+            model="Organika/sdxl-detector"
         )
-        
+
         image = Image.open(image_path).convert("RGB")
         results = detector(image)
-        
+
         ai_score = 0.0
         human_score = 0.0
-        
+
         for result in results:
-         label = result["label"].lower()
-         if "artificial" in label or "ai" in label or "fake" in label or "generated" in label:
-          ai_score = result["score"]
-         else:
-          human_score = result["score"]
-        
+            label = result["label"].lower()
+            print(f"Label: {label}, Score: {result['score']}")
+            if "artificial" in label or "sdxl" in label or "fake" in label or "generated" in label:
+                ai_score = result["score"]
+            else:
+                human_score = result["score"]
+
         is_ai = ai_score > human_score
         confidence = round(max(ai_score, human_score), 2)
-        
+
         if is_ai and confidence > 0.8:
             risk = "high"
         elif is_ai:
             risk = "moderate"
         else:
             risk = "safe"
-        
+
         return {
             "is_ai_generated": is_ai,
             "confidence": confidence,
             "risk_level": risk,
             "verdict": "AI Generated" if is_ai else "Real Image"
         }
-        
+
     except Exception as e:
         return {"error": str(e)}
